498713a1c13ae79646555d9760d92cdbf497433f
parzival-3x <bg193b@gmail.com>
2023-03-09 17:00:12 -0600

    Started making front page

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- https://www.w3schools.com/php/php_mysql_select.asp -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>MFAH</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
